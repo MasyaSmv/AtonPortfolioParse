cfed429d90f225b828d37f5aad3fd11f6f0d2995
MasyaSm <kuvaldamax@gmail.com>
2024-02-06 22:17:49 +0300

    upd composer file + add test file

---
+++ src/ParseAtonFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aton\Portfolio\Parse;
+
+class ParseAtonFactory
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function load($file)
+    {
+//        $file = File::assertFile($file);
+    }
+}
